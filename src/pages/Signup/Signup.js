5b8ef54a4125387449604efaf93648c0ce67b998
Rafique <rasgalazy5@gmail.com>
2021-04-05 21:02:27 +0000

    Sign up

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
-import Mycss1 from "../Signup/Signup.module.css"
-import Button from "../../components/appcomponents/button"
-import InputField from "../../components/appcomponents/InputField"
-import pic1 from "./img/login-green2.svg"
-import pic3 from "../Login/img/avatar.svg"
-
-
+import Mycss1 from "../Signup/Signup.module.css";
+import Button from "../../components/appcomponents/button";
+import InputField from "../../components/appcomponents/InputField";
+import pic1 from "./img/login-green2.svg";
+import pic3 from "../Login/img/avatar.svg";
 
 function Signup() {
-    return (
-        <>
-     <img className={Mycss1.wave} src={pic1}/>
-    <div className={Mycss1.container}>
-        <div className={Mycss1.img}/>
+
+
+
+
+
+
+  return (
+    <>
+      <img className={Mycss1.wave} src={pic1} />
+      <div className={Mycss1.container}>
+        <div className={Mycss1.img} />
         <div className={Mycss1.login_content}>
           <form className={Mycss1.form_control}>
-            <img src={pic3}/>
+            <img src={pic3} />
             <h2 className={Mycss1.title}>Welcome</h2>
 
-            {/* Username section */}
+            {/* Name section */}
             <div className={Mycss1.input_div}>
               <div className={Mycss1.divdiv}>
                 <InputField
@@ -26,52 +30,55 @@ function Signup() {
                   placeholder="Name"
                   type="text"
                   className={Mycss1.input}
-                //   onChange={Updateusername}
+                  //   onChange={Updateusername}
                 />
               </div>
             </div>
 
-            {/* Password Section */}
+            {/* Email Section */}
+            <div className={Mycss1.input_div}>
+              <div className={Mycss1.div}>
+                <InputField
+                  placeholder="Email"
+                  type="email"
+                  className={Mycss1.input}
+                  //   onChange={Updatepassword}
+                  // value={password}
+                />
+              </div>
+            </div>
+            {/* Username Session */}
+            <div className={Mycss1.input_div}>
+              <div className={Mycss1.div}>
+                <InputField
+                  placeholder="Username"
+                  type="text"
+                  className={Mycss1.input}
+                  //   onChange={Updatepassword}
+                  // value={password}
+                />
+              </div>
+            </div>
+            {/* Password Session */}
             <div className={Mycss1.input_div}>
               <div className={Mycss1.div}>
                 <InputField
                   placeholder="Password"
                   type="password"
                   className={Mycss1.input}
-                //   onChange={Updatepassword}
-                  // value={password}
-                />
-              </div>
-                        </div>
-                        <div className={Mycss1.input_div}>
-              <div className={Mycss1.div}>
-                <InputField
-                  placeholder="Password"
-                  type="password"
-                  className={Mycss1.input}
-                //   onChange={Updatepassword}
+                  //   onChange={Updatepassword}
                   // value={password}
                 />
               </div>
             </div>
-                        <div className={Mycss1.input_div}>
+            {/* Confirm Password Session */}
+            <div className={Mycss1.input_div}>
               <div className={Mycss1.div}>
                 <InputField
-                  placeholder="Password"
+                  placeholder="Confirm Password"
                   type="password"
                   className={Mycss1.input}
-                //   onChange={Updatepassword}
-                  // value={password}
-                />
-              </div>
-                        </div>
-                        <div className={Mycss1.input_div}>
-              <div className={Mycss1.div}>
-                <InputField
-                  placeholder="Password"
-                  type="password"
-                  className={Mycss1.input}
-                //   onChange={Updatepassword}
+                  //   onChange={Updatepassword}
                   // value={password}
                 />
               </div>
@@ -79,15 +86,16 @@ function Signup() {
 
             <a href="#">Forgot Password?</a>
 
-            <Button className=""
+            <Button
+              className=""
               // onClick={() => gotoPage("/Signup")}
-              text="Login" />
+              text="Signup"
+            />
           </form>
         </div>
       </div>
-        </>
-    )
+    </>
+  );
 }
-
 
 export default Signup;
